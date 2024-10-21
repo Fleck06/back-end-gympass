1f93c662e53e86b364577fc4f6c6886f2ad8f6fd
Mateus Fleck <mateus.fleck@duque.g12.br>
2024-10-21 20:58:59 -0300

    Fist Commit

---
+++ src/repositories/in-memory-check-ins-repository.ts
@@ -0,0 +1,19 @@
+import type { CheckIn, Prisma } from "@prisma/client";
+import type { CheckInsRepository } from "./check-ins-repository";
+import { randomUUID } from "node:crypto";
+
+export class InMemoryCheckInsRepository implements CheckInsRepository{
+  public items:CheckIn[] = [] // lista vazia que representa o banco de dados
+  async create(data: Prisma.CheckInUncheckedCreateInput): Promise<CheckIn> {
+    const checkIn = {
+      id: randomUUID(),
+      user_id: data.user_id,
+      gym_id: data.gym_id,
+      validated_at: data.validated_at ? new Date(data.validated_at):null,
+      created_at: new Date()
+    }
+    this.items.push(checkIn)
+    return checkIn
+  }
+
+}
